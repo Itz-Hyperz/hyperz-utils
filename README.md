b755297a1c19fe0f1002b70114604f2357c6720b
Itz-Hyperz <loganfgross@gmail.com>
2022-10-14 12:09:01 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# hyperz-utils
+# Hyperz Utilities
+A simple utilities package that offers many different functions that could be of use to you when writing ExpressJS websites or Discord bots!
+
+---
+
+# Functions / Exports
+- [figlify](#figlify)
+- [mdConvert](#mdConvert)
+- [generateRandom](#generateRandom)
+- [sanitize](#sanitize)
+- [saveFile](#saveFile)
+- [getDate](#fetchTime)
+- [fetchTime](#fetchTime)
+- [getRandomArray](#getRandomArray)
+- [checkIfHex](#checkIfHex)
+- [dirSize](#dirSize)
+
+---
+
+# figlify
+Used to turn text into art!
+```js
+    const utils = require('hyperz-utils');
+    await utils.figlify("Hello World!", { randFont: true, font: undefined });
+```
+
+# mdConvert
+Converts raw text/md to HTML compatible.
+```js
+    const utils = require('hyperz-utils');
+    let text = "# Hello world!";
+    let webPageContent = await utils.mdConvert(text);
+    res.render('index.ejs', { content: webPageContent }); // ExpressJS Framework Example
+```
+
+# generateRandom
+Generate a random string of acceptable characters.
+```js
+    const utils = require('hyperz-utils');
+    let random = await utils.generateRandom(12) // How long the string should be
+    console.log(random);
+```
+
+# sanitize
+Make a string of text MySQL compatible to avoid injections.
+```js
+    const utils = require('hyperz-utils');
+    let text = "oh hello world`; DROP DATABASE main;";
+    let cleansed = await utils.sanitize(text, false) // boolean for <script> replacement in HTML
+    await con.query(cleansed, function(err, row) {
+        if(err) throw err;
+    });
+```
+
+# saveFile
+Download and save a file from a URL. (NodeJS File Downloader)
+```js
+    const utils = require('hyperz-utils');
+    utils.saveFile("https://hyperz.net/assets/logo.png", "hyperzlogo", "png", "./public/images", false); // boolean is to clone files or not
+```
+
+# getDate
+Get the date and convert to string with NodeJS.
+```js
+    const utils = require('hyperz-utils');
+    let date = await utils.getDate();
+    console.log(date);
+```
+
+# fetchTime
+Fetch the current time from a certain time zone.
+```js
+    const utils = require('hyperz-utils');
+    let time = await utils.fetchTime("America/New_York", "MM-DD-YYYY"); // https://github.com/Itz-Hyperz/big-ben-bot/blob/main/timezones.json look at values
+    console.log(date);
+```
+
+# getRandomArray
+Get a random element from an array of strings.
+```js
+    const utils = require('hyperz-utils');
+    let array = ["a", "b", "c", "d", "e", "f"];
+    let random = await utils.getRandomArray(array);
+    console.log(`you got: ${random}!`);
+```
+
+# checkIfHex
+Check if the provided string is a valid color hex.
+```js
+    const utils = require('hyperz-utils');
+    let hex = "#FFFFFF";
+    let check = await utils.checkIfHex(hex);
+    if(!check.pass) hex = check.item; // Set to the proper hex value if it's not
+    console.log(hex);
+```
+
+# dirSize
+View the directory size of a folder.
+```js
+    const utils = require('hyperz-utils');
+    let size = await utils.dirSize("./src/images");
+    console.log(size);
+```
+
+---
+
+# Credits
+[@Hyperz](https://hyperz.net) - *Creating the package.*
---
+++ main.js
@@ -0,0 +1,118 @@
+const moment = require('moment-timezone');
+const filedl = require("nodejs-file-downloader");
+const figlet = require('figlet');
+const fastFolderSizeSync = require('fast-folder-size/sync');
+const converter = require('byte-converter').converterBase2;
+const md = require('markdown-it-faxes')({
+    html: false, // Enable HTML tags in source
+    xhtmlOut: false, // Use '/' to close single tags (<br />).
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: false // Autoconvert URL-like text to links
+}).use(require('markdown-it-highlightjs'), { code: true });
+let defaultFonts = ["Graffiti", "Standard", "Stop", "Slant", "Pagga", "Larry 3D"];
+
+async function figlify(text, options) {
+    if(typeof options.font == 'undefined') {
+        if(typeof options.randFont == 'undefined' || options.randFont == false) {
+            figlet.text(text, { font: 'Standard', width: options.width || 700 }, function(err, data) {
+                if(err) throw err;
+                let str = `${data}\n-------------------------------------------`
+                console.log(chalk.bold(chalk.blueBright(str)));
+            });
+        } else {
+            let chosen = await getRandomArray(defaultFonts);
+            figlet.text(text, { font: chosen, width: options.width || 700 }, function(err, data) {
+                if(err) throw err;
+                let str = `${data}\n-------------------------------------------`
+                console.log(chalk.bold(chalk.blueBright(str)));
+            });
+        }
+    } else {
+        figlet.text(text, { font: options.font, width: options.width || 700 }, function(err, data) {
+            if(err) throw err;
+            let str = `${data}\n-------------------------------------------`
+            console.log(chalk.bold(chalk.blueBright(str)));
+        });
+    }
+};
+
+async function mdConvert(content) {
+    let rendered = await md.render(content);
+    return rendered;
+};
+
+async function generateRandom(length) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
+
+async function sanitize(value, bypassScripting) {
+    if(!bypassScripting || typeof bypassScripting == 'undefined') {
+        if(value.toLowerCase().includes('</')) {
+            value = await value.replaceAll('<', 'NULLED:lessThan').replaceAll('>', 'NULLED:greaterThan');
+        };
+    };
+    value = await value.replaceAll('"', '\'').replaceAll('`', '\`').replaceAll("'", "\'");
+    return value;
+};
+
+async function saveFile(url, name, type, dir, clonefiles) {
+    try {
+        const downloader = new filedl({
+            url: url, //If the file name already exists, a new file with the name 200MB1.zip is created.
+            directory: dir, //This folder will be created, if it doesn't exist.
+            fileName: `${name}.${type}`,
+            cloneFiles: clonefiles || false
+        });
+        await downloader.download();
+    } catch(e) {};
+};
+
+async function getDate() {
+    let date = new Date().toDateString();
+    return date;
+};
+
+async function fetchTime(tz, format) {
+    let datethingy = moment.tz(tz).format(format);
+    return datethingy;
+};
+
+async function getRandomArray(array) {
+    let bruh = array[Math.floor(array.length * Math.random())];
+    return bruh;
+};
+
+async function checkIfHex(item) {
+    if(item.toUpperCase().startsWith('#')) {
+        return { pass: true };
+    } else {
+        return { pass: false, item: `#${item}` };
+    };
+};
+
+async function dirSize(directory) {
+    let final;
+    let bytes = fastFolderSizeSync(directory);
+    final = await converter(bytes, 'B', 'MB'); // Bytes to MB
+    final = Number(final).toFixed(2);
+    return final;
+};
+
+module.exports = {
+    figlify: figlify,
+    mdConvert: mdConvert,
+    generateRandom: generateRandom,
+    sanitize: sanitize,
+    saveFile: saveFile,
+    getDate: getDate,
+    fetchTime: fetchTime,
+    getRandomArray: getRandomArray,
+    checkIfHex: checkIfHex,
+    dirSize: dirSize
+};
